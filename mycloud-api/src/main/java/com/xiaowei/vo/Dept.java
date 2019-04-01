fd1837c73b04bd3a36e592654d7d41682e697ab3
caoxiaowei414 <337619617@qq.com>
2019-04-01 22:58:24 +0800

    Initial commit

---
+++ mycloud-api/src/main/java/com/xiaowei/vo/Dept.java
@@ -0,0 +1,11 @@
+package com.xiaowei.vo;
+
+import java.io.Serializable;
+
+
+
+public class Dept implements Serializable {
+    private Long deptno ;
+    private String dname ;
+    private String loc ;
+}
